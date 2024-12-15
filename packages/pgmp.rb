5b5ad2eb78c2c14ac5aba3fb27a5340c6cb106ed
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-15 14:47:44 +0700

    Problem: pgmp won't build
    Solution: supply the dependencies it needs

--- packages/pgmp.rb
+++ packages/pgmp.rb
@@ -2,4 +2,12 @@
 
 class Pgmp < Pgpm::Package
   github "dvarrazzo/pgmp", tag_prefix: "rel-"
+
+  def build_dependencies
+    super + %w(gmp-devel)
+  end
+
+  def dependencies
+    super + %w(gmp)
+  end
 end
