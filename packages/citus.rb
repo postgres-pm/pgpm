e1694da9fa7ccd81c0304d8b78b36bd7cc1969a8
Yurii Rashkovskii <yrashk@gmail.com>
2024-12-23 18:57:51 +0700

    Problem: citus won't build
    Solution: correct the dependencies and build process
    
    Also, mark it as broken on Postgres above 16, as it isn't supported yet.
    (See https://github.com/citusdata/citus/issues/7708)

--- packages/citus.rb
+++ packages/citus.rb
@@ -2,4 +2,21 @@
 
 class Citus < Pgpm::Package
   github "citusdata/citus"
+
+  def build_steps
+    ["./configure"] + super
+  end
+
+  def build_dependencies
+    super + %w[libcurl-devel lz4-devel libzstd-devel openssl-devel krb5-devel]
+  end
+
+  def dependencies
+    super + %w[libcurl lz4 libzstd openssl krb5-libs]
+  end
+
+  def broken?
+    # https://github.com/citusdata/citus/issues/7708
+    Pgpm::Postgres::Distribution.in_scope.major_version > 16
+  end
 end
