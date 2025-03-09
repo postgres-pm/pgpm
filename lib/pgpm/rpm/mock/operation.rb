7168b5137029b2837987a4c73c9c6b45de820419
Yurii Rashkovskii <yrashk@gmail.com>
2025-03-09 05:59:47 -0700

    Problem: building extensions that require other extensions
    This doesn't currently work because we try to install those other extensions in
    the last build and they are not available.
    
    Solution: build them together with the package
    
    The implementation is currently very, very hacky – that Proc refinement is just
    a massive hack. I've also fixed rebuild chaining but it is extremely brittle as
    well.
    
    But at least it works. I think.

--- lib/pgpm/rpm/mock/operation.rb
+++ lib/pgpm/rpm/mock/operation.rb
@@ -68,24 +68,19 @@ module Pgpm
         def chain(op)
           raise ArgumentError, "can't chain non-rebuild operations" unless op.args.include?("--rebuild") && @args.include?("--rebuild")
 
-          args = @args.clone
-          args.insert(@args.index("--localrepo") - 1, op.args[op.args.index("--localrepo") - 1])
-          self.class.new(*args, cb: lambda {
-            res1 = @cb&.call
-            res2 = op.cb&.call
-            return res1 + res2 if res1.is_a?(Array) && res2.is_a?(Array)
-
-            res2
-          })
+          self.args.insert(self.args.index("--localrepo"), *op.args[op.args.index("--recurse") + 1..op.args.index("--localrepo")-1])
+          self
         end
 
         def and_then(op)
           lambda do
             res1 = call
             res2 = op.call
-            return res1 + res2 if res1.is_a?(Array) && res2.is_a?(Array)
-
-            [res1, res2]
+            if res1.is_a?(Array) && res2.is_a?(Array)
+              res1 + res2
+            else
+              [res1, res2]
+            end
           end
         end
       end
