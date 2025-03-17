48492aeae761e95c03d3b48f5fd46c23177450a0
Yurii Rashkovskii <yrashk@gmail.com>
2025-03-17 17:34:38 -0400

    Problem: building RPMs with mock is quite slow
    Especially when there is a lot of dependencies.
    
    Solution: ensure we order them correctly
    
    This way, mock doesn't have to dynamically re-retry when it is missing
    dependencies.

--- lib/pgpm/package/dependencies.rb
+++ lib/pgpm/package/dependencies.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "tsort"
+
 module Pgpm
   class Package
     module Dependencies
@@ -19,6 +21,33 @@ module Pgpm
 
       def all_requirements
         requires.flat_map { |r| [r, *r.all_requirements] }.uniq
+      end
+
+      def topologically_ordered_with_dependencies
+        TopologicalPackageSorter.new([self, *all_requirements]).sorted_packages
+      end
+
+      class TopologicalPackageSorter
+        include TSort
+
+        def initialize(packages)
+          @packages = packages.each_with_object({}) do |pkg, hash|
+            hash[pkg.name] = pkg
+          end
+        end
+
+        def tsort_each_node(&block)
+          @packages.each_key(&block)
+        end
+
+        def tsort_each_child(node, &block)
+          package = @packages[node]
+          package.requires.each { |req| block.call(req) if @packages.key?(req) }
+        end
+
+        def sorted_packages
+          tsort.map { |name| @packages[name] }.reverse
+        end
       end
 
       def c_files_present?
